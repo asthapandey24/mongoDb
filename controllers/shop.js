f7235537a4a5137bf61f5d058278601bdeab9b6e
ThrivingDep <119737884+ThrivingDep@users.noreply.github.com>
2023-08-02 18:40:23 +0530

    orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -126,29 +126,30 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
-  req.user
-    .getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then(products => {
-      return req.user
-        .createOrder()
-        .then(order => {
-          return order.addProducts(
-            products.map(product => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .catch(err => console.log(err));
-    })
-    .then(result => {
-      return fetchedCart.setProducts(null);
-    })
+   let fetchedCart;
+   req.user
+   .addOrder()
+  //   .getCart()
+  //   .then(cart => {
+  //     fetchedCart = cart;
+  //     return cart.getProducts();
+  //   })
+  //   .then(products => {
+  //     return req.user
+  //       .createOrder()
+  //       .then(order => {
+  //         return order.addProducts(
+  //           products.map(product => {
+  //             product.orderItem = { quantity: product.cartItem.quantity };
+  //             return product;
+  //           })
+  //         );
+  //       })
+  //       .catch(err => console.log(err));
+  //   })
+  //   .then(result => {
+  //     return fetchedCart.setProducts(null);
+  //   })
     .then(result => {
       res.redirect('/orders');
     })
@@ -157,7 +158,7 @@ exports.postOrder = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({include: ['products']})
+    .getOrders()
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
