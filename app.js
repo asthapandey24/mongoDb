7ffc17f4ce6c41c41bd8c85f9fe8e76c0f49b077
ThrivingDep <119737884+ThrivingDep@users.noreply.github.com>
2023-08-04 19:55:08 +0530

    Mongoose

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect; // by doing this we can connect with databse as well as can do insert or other operation
-const User = require('./models/user');
+//const mongoConnect = require('./util/database').mongoConnect; // by doing this we can connect with databse as well as can do insert or other operation
+//const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,26 +18,29 @@ const adminRoutes = require('./routes/admin');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-   User.findById('64c8af67a5732937071564c8')
-     .then(user => {
-       req.user = new User (user.name, user.email, user.cart, user._id);
-       next();
-     })
-     .catch(err => console.log(err));
-   // next();
-});
+// app.use((req, res, next) => {
+//    User.findById('64c8af67a5732937071564c8')
+//      .then(user => {
+//        req.user = new User (user.name, user.email, user.cart, user._id);
+//        next();
+//      })
+//      .catch(err => console.log(err));
+//    // next();
+// });
 
 app.use('/admin', adminRoutes);
  app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
- // console.log(client);
+mongoose
+.connect(
+  'mongodb+srv://astha24:Astha%4024@cluster0.7z9y5j7.mongodb.net/'
+  ).then(result =>{
   app.listen(3000);
-});
-
+}).catch(err =>{
+  console.log(err);
+})
 
 
 
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
-const mongodb = require('mongodb');
+//const mongodb = require('mongodb');
 
 const Product = require('../models/product');
-const ObjectId = mongodb.ObjectId;
+//const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -16,7 +16,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl, null, req.user._id )
+  const product = new Product({
+    title: title,
+     price: price, 
+     description: description,
+     imageUrl: imageUrl
+    })
   // req.user
   //   .createProduct({
   //     title: title,
@@ -72,15 +77,20 @@ exports.postEditProduct = (req, res, next) => {
       // product.price = updatedPrice;
       // product.description = updatedDesc;
       // product.imageUrl = updatedImageUrl;
-      const product = new Product(updatedTitle, 
-         updatedPrice,
-         updatedDesc, 
-         updatedImageUrl,
-         new ObjectId(prodId))
+      // const product = new Product(updatedTitle, 
+      //    updatedPrice,
+      //    updatedDesc, 
+      //    updatedImageUrl,
+      //    new ObjectId(prodId))
       //return product.save();
     //})
-    product
-    .save()
+    Product.findById(prodId).then(product =>{
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+     return  product.save();
+    })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -89,7 +99,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -103,7 +113,7 @@ exports.getProducts = (req, res, next) => {
  exports.postDeleteProduct = (req, res, next) => {
    const prodId = req.body.productId;
   //  Product.findById(prodId)
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     //  .then(product => {
     //    return product.destroy();
     //  })
