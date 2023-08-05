a0c96c55a87059ab09c194be1750700a74ca3177
ThrivingDep <119737884+ThrivingDep@users.noreply.github.com>
2023-08-05 12:20:37 +0530

    CartSystem

--- controllers/shop.js
+++ controllers/shop.js
@@ -53,8 +53,12 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+  .populate('cart.items.productId')
+  .execPopulate()
+    //.getCart()
+    .then(user => {
+      //console.log(user.cart.items);
+      const products = user.cart.items
       // return products
       //   .getProducts()
       //   .then(products => {
@@ -77,37 +81,37 @@ exports.postCart = (req, res, next) => {
       res.redirect('/cart')
     })
   })
-   let fetchedCart;
-   let newQuantity = 1;
-   req.user
-    .getCart()
-    .then(cart => {
-            fetchedCart = cart;
-      return cart.getProducts({ where: { id: prodId } });
-     })
-     .then(products => {
-      let product;
-            if (products.length > 0) {
-         product = products[0];
-       }
+//    let fetchedCart;
+//    let newQuantity = 1;
+//    req.user
+//     .getCart()
+//     .then(cart => {
+//             fetchedCart = cart;
+//       return cart.getProducts({ where: { id: prodId } });
+//      })
+//      .then(products => {
+//       let product;
+//             if (products.length > 0) {
+//          product = products[0];
+//        }
 
-      if (product) {
-        const oldQuantity = product.cartItem.quantity;
-        newQuantity = oldQuantity + 1;
-         return product;
-       }
-       return Product.findById(prodId);
-     })
-     .then(product => {
-      return fetchedCart.addProduct(product, {
-        through: { quantity: newQuantity }
-       });
-     })
-     .then(() => {
-       res.redirect('/cart');
-   })
-     .catch(err => console.log(err));
-};
+//       if (product) {
+//         const oldQuantity = product.cartItem.quantity;
+//         newQuantity = oldQuantity + 1;
+//          return product;
+//        }
+//        return Product.findById(prodId);
+//      })
+//      .then(product => {
+//       return fetchedCart.addProduct(product, {
+//         through: { quantity: newQuantity }
+//        });
+//      })
+//      .then(() => {
+//        res.redirect('/cart');
+//    })
+//      .catch(err => console.log(err));
+ };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
