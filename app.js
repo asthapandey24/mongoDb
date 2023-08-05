c9119886ebfd498d3715aaeac67a370bea5b51ec
ThrivingDep <119737884+ThrivingDep@users.noreply.github.com>
2023-08-05 11:28:01 +0530

    UserTable

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 //const mongoConnect = require('./util/database').mongoConnect; // by doing this we can connect with databse as well as can do insert or other operation
-//const User = require('./models/user');
+const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -14,19 +14,20 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
  const shopRoutes = require('./routes/shop');
+const user = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//    User.findById('64c8af67a5732937071564c8')
-//      .then(user => {
-//        req.user = new User (user.name, user.email, user.cart, user._id);
-//        next();
-//      })
-//      .catch(err => console.log(err));
-//    // next();
-// });
+ app.use((req, res, next) => {
+    User.findById('64cdd90df903b8d1b22ca336')
+     .then(user => {
+       req.user = user
+       next();
+     })
+     .catch(err => console.log(err));
+   // next();
+ });
 
 app.use('/admin', adminRoutes);
  app.use(shopRoutes);
@@ -37,6 +38,18 @@ mongoose
 .connect(
   'mongodb+srv://astha24:Astha%4024@cluster0.7z9y5j7.mongodb.net/'
   ).then(result =>{
+    User.findOne().then(user =>{
+      if(!user){
+        const user = new User({
+          name: 'astha',
+          email: 'kuchv@mail.com',
+          cart:{
+            items: []
+          }
+        })
+        user.save()
+      }
+    })
   app.listen(3000);
 }).catch(err =>{
   console.log(err);
--- controllers/admin.js
+++ controllers/admin.js
@@ -20,7 +20,8 @@ exports.postAddProduct = (req, res, next) => {
     title: title,
      price: price, 
      description: description,
-     imageUrl: imageUrl
+     imageUrl: imageUrl,
+     userId: req.user
     })
   // req.user
   //   .createProduct({
@@ -100,7 +101,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+//   .select('title price - _id')
+//  .populate('userId', 'name')
     .then(products => {
+       console.log(products)
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
